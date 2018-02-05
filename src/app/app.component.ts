4f55be4dcb7a6fa084986f4130bd7a4366b75ebf
Sirui Chen <632882184@qq.com>
2018-02-05 18:37:46 +0800

    add some DI to see what it is compiled into; remove useless html; translate Chinese into English

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AppCompService } from './appComp.services';
+import { AppModuleService } from './appModule.services';
+
 declare var $: any;
 
 @Component({
@@ -10,31 +13,31 @@ declare var $: any;
         Welcome to {{title}}!
       </h1>
       <h2>{{ChangeDetect}}</h2>
-      <h2 id="jqRed"></h2>
+      <h2 id="show-jquery"></h2>
+      <h2>{{appModuleServiceData}}</h2>
+      <h2>{{appCompServiceData}}</h2>
       <img width="300" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
     </div>
-    <h2 class="start-links">Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://github.com/angular/angular-cli/wiki">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
   `,
   styleUrls: ['app.component.css'],
-  styles: ['.start-links {color: green;}']
+  styles: ['.welcome {background-color: gray;}'],
+  providers: [AppCompService]
 })
 export class AppComponent implements OnInit {
-  title = 'app';
+  title = 'aot demo';
   ChangeDetect = '';
+
+  appCompServiceData = '';
+  appModuleServiceData = '';
+
+  constructor(appCompService: AppCompService,
+    appModuleService: AppModuleService) {
+    this.appCompServiceData = appCompService.data;
+    this.appModuleServiceData = appModuleService.data;
+  }
+
   ngOnInit() {
-    $('#jqRed').text("jquery已经成功引入").css("color", "red");
-    setTimeout(() => this.ChangeDetect += 'Angular变化检测正常', 1500);
+    $('#show-jquery').text("import jquery successfully!").css("color", "red");
+    setTimeout(() => this.ChangeDetect = 'Angular change detection works!', 1500);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { AppModuleService } from './appModule.services';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [AppModuleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
